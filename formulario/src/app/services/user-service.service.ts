9c50434d59b489746e5f173670a85911892f76bb
Violetaaa <Valvarezareces@gmail.com>
2021-07-02 17:21:26 +0200

    added a Procfile

--- formulario/src/app/services/user-service.service.ts
+++ formulario/src/app/services/user-service.service.ts
@@ -7,29 +7,32 @@ import { Person } from '../interfaces/person';
   providedIn: 'root'
 })
 export class UserService {
-  url='http://localhost:3000/users/';
+  // url='http://localhost:3000/users/';
+  url = 'users/';
 
 
-  constructor(private http: HttpClient) { 
+
+
+  constructor(private http: HttpClient) {
   }
 
-  getUsers(): Observable<any>{
+  getUsers(): Observable<any> {
     return this.http.get(this.url);
   }
 
-  getUser(id: number): Observable <any>{
+  getUser(id: number): Observable<any> {
     return this.http.get(this.url + id);
   }
 
-  postUser(person: Person): Observable<any>{
+  postUser(person: Person): Observable<any> {
     return this.http.post(this.url, person);
   }
 
-  deleteUser(id: number): Observable <any>{
+  deleteUser(id: number): Observable<any> {
     return this.http.delete(this.url + id);
   }
 
-  updateUser(id: number, person: Person): Observable <any>{
+  updateUser(id: number, person: Person): Observable<any> {
     return this.http.put(this.url + id, person);
   }
 
